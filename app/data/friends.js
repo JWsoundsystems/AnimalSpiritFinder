8f5224a8f24e88e1b31d611179c856b24af48fbb
JWsoundsystems <JWsoundsystems@gmail.com>
2020-01-13 23:07:36 -0800

    add api functionality and request functionality

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+  {
+    name: "Dog",
+    photo:
+      "https://vetstreet.brightspotcdn.com/dims4/default/fa9df8a/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Ffa%2F60ac50a80411e0a0d50050568d634f%2Ffile%2FShiba-Inu-3-645mk062811.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Cat",
+    photo:
+      "https://media.gettyimages.com/photos/persian-picture-id537312023?s=612x612",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+  {
+    name: "Lizard",
+    photo:
+      "https://wamu.org/wp-content/uploads/2019/06/20181114-LIZARD-FENSTON-08-1500x1000.jpg",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+  {
+    name: "Horse",
+    photo:
+      "https://ichef.bbci.co.uk/news/1024/cpsprodpb/1245A/production/_103224847_horsea.png",
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+  {
+    name: "Bird",
+    photo:
+      "http://www.dailynews.lk/sites/default/files/news/2017/08/13/z_p30-Scientists.jpg",
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+  {
+    name: "Fish",
+    photo:
+      "https://images.unsplash.com/photo-1524704654690-b56c05c78a00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+  },
+  {
+    name: "Moose",
+    photo:
+      "https://cdn.britannica.com/57/92857-050-8D5A0A8E/bull-moose-water.jpg",
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  }
+];
+module.exports = friends;
